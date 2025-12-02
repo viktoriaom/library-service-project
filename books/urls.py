cfc752ff9c7e1bb58929dac80c8e51c6dd560d53
Vik <viktoria.om@gmail.com>
2025-12-02 22:44:01 +0200

    create urls.py for books app

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from books.views import BookViewSet
+
+router = DefaultRouter()
+router.register("books", BookViewSet)
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "books"
