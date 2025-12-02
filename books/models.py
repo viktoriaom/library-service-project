2a73c4cc401e0689ec2f6bf6314f09a44bab136e
Vik <viktoria.om@gmail.com>
2025-12-02 22:40:54 +0200

    create Book model & register it in admin.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=4,
+                             choices=Cover.choices,
+                             default=Cover.HARD
+                             )
+    inventory = models.PositiveIntegerField()
+    daily_fee = MoneyField(
+        max_digits=10,
+        decimal_places=2,
+        default_currency="USD",
+        validators=[MinValueValidator(0)]
+    )
