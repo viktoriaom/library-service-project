a1f5af52d3f78708150e9bae2d5fd4e1a39c3a13
Vik <viktoria.om@gmail.com>
2025-12-04 19:36:11 +0200

    add borrowings create url

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from borrowings.views import BorrowingsReadSet
+from borrowings.views import BorrowingsReadSet, BorrowingsCreateView
 
 router = DefaultRouter()
 router.register("borrowings", BorrowingsReadSet)
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls)),
+               path("create/", BorrowingsCreateView.as_view(), name="borrowings-create"),
+               ]
 
 app_name = "borrowings"
