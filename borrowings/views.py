fc48bebdeaf35a5fe690975137ed6661b9e883d4
Vik <viktoria.om@gmail.com>
2025-12-04 19:35:33 +0200

    create BorrowingCreateSerializer & BorrowingsCreateView

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,33 @@
-from rest_framework import viewsets
+from django.db import transaction
+from rest_framework import viewsets, generics
+from rest_framework.exceptions import ValidationError
 
+from books.models import Book
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer
+)
 
 
 class BorrowingsReadSet(viewsets.ReadOnlyModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+
+class BorrowingsCreateView(generics.CreateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingCreateSerializer
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            book = serializer.validated_data['book']
+            book = Book.objects.select_for_update().get(id=book.id)
+            if book.inventory > 0:
+                book.inventory -= 1
+                book.save()
+                serializer.save(user=self.request.user)
+            else:
+                raise ValidationError(
+                    {"book": "Unfortunately, no copies left to borrow."}
+                )
