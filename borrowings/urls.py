9a40705dd150b739cfa3e9b816df0b5d17be8e93
Vik <viktoria.om@gmail.com>
2025-12-07 16:51:51 +0200

    add borrowings return url

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from borrowings.views import BorrowingsReadSet, BorrowingsCreateView
+from borrowings.views import (
+    BorrowingsReadSet,
+    BorrowingsCreateView,
+    BorrowingsReturnView
+)
 
 router = DefaultRouter()
 router.register("borrowings", BorrowingsReadSet)
 urlpatterns = [path("", include(router.urls)),
-               path("create/", BorrowingsCreateView.as_view(), name="borrowings-create"),
+               path("create/",
+                    BorrowingsCreateView.as_view(),
+                    name="borrowings-create"),
+               path("<int:pk>/return/",
+                    BorrowingsReturnView.as_view(),
+                    name="borrowings-return"
+                    )
                ]
 
 app_name = "borrowings"
