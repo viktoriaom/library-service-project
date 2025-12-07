a63baa1220e2177b7b9ad401ff4a609bfbd85e46
Vik <viktoria.om@gmail.com>
2025-12-07 17:30:35 +0200

    Revert "create BorrowingReturnSerializer & BorrowingsReturnView"
    This reverts commit 40096400c43d7da03e5569edb78df4f5399b91c7.
    
    revert wrong changes

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,9 +1,7 @@
-from django.utils import timezone
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
-from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -30,38 +28,3 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "book",
         ]
-
-
-class BorrowingReturnSerializer(serializers.ModelSerializer):
-    book = BookSerializer(read_only=True)
-    user = UserSerializer(read_only=True)
-    fee_to_pay = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Borrowing
-        fields = [
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book",
-            "user",
-            "fee_to_pay",
-        ]
-        read_only_fields = [
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book",
-            "user",
-            "fee_to_pay",
-        ]
-
-    def get_fee_to_pay(self, obj):
-        return_date = obj.actual_return_date if (
-            obj.actual_return_date) else (
-            timezone.now().date())
-        days_borrowed = (return_date - obj.borrow_date).days
-        total_fee = days_borrowed * float(obj.book.daily_fee.amount)
-        return round(total_fee, 2)
