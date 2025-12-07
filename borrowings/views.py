40096400c43d7da03e5569edb78df4f5399b91c7
Vik <viktoria.om@gmail.com>
2025-12-07 16:51:13 +0200

    create BorrowingReturnSerializer & BorrowingsReturnView

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,13 +1,16 @@
+from django.utils import timezone
+
 from django.db import transaction
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingSerializer,
-    BorrowingCreateSerializer
+    BorrowingCreateSerializer, BorrowingReturnSerializer
 )
 
 
@@ -57,3 +60,45 @@ class BorrowingsCreateView(generics.CreateAPIView):
                 raise ValidationError(
                     {"book": "Unfortunately, no copies left to borrow."}
                 )
+
+
+class BorrowingsReturnView(generics.RetrieveUpdateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReturnSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Borrowing.objects.filter(actual_return_date__isnull=True)
+        return Borrowing.objects.filter(
+            user=self.request.user,
+            actual_return_date__isnull=True
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.actual_return_date is not None:
+            raise ValidationError({
+                "detail": "This borrowing has already been returned."
+            })
+
+        with transaction.atomic():
+            book = instance.book
+            book.inventory += 1
+            book.save()
+
+            instance.actual_return_date = timezone.now().date()
+            instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(
+            {"message": "Book returned successfully",
+             "borrowing": serializer.data
+             },
+            status=status.HTTP_200_OK
+        )
+
+    def partial_update(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
